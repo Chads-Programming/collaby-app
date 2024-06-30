f862f77636e418e5cee6837fae97d5c71879966e
Arturo Iván Artaza <aartazaesc@gmail.com>
2024-06-30 01:52:38 -0300

    fix(ui): kirbe mistakes

--- src/app/(jobs)/page.tsx
+++ src/app/(jobs)/page.tsx
@@ -1,17 +1,16 @@
 import Image from "next/image";
 import { NavBar } from "@/app/components";
 import { Hero, JobCard } from "@/app/(jobs)/components";
-import { FiltersSidebar } from "./components/filters-sidebar/filters-sidebar";
+import { Filter } from "./components/filters-sidebar/filters-sidebar";
 
 export default async function JobsPage() {
   return (
-    <main className="flex min-h-screen flex-col bg-gradient-to-b text-white bg-muted">
+    <main className="flex min-h-screen flex-col bg-gradient-to-b text-white bg-background">
       <NavBar />
       <section>
         <Hero />
-        <FiltersSidebar />
+        <Filter />
       </section>
-      <Hero />
 
       <JobCard
         logo="https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png"

e86cfe93b08a6e921802ab1d673643340a9d3f8c
trivago123450 <trivago123450@gmail.com>
2024-06-30 05:28:19 +0000

    feat: pagination component

--- src/app/(jobs)/page.tsx
+++ src/app/(jobs)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { NavBar } from "@/app/components";
-import { Hero, JobCard } from "@/app/(jobs)/components";
+import { Hero, JobCard, Pagination } from "@/app/(jobs)/components";
 import { Filter } from "./components/filters-sidebar/filters-sidebar";
 
 export default async function JobsPage() {
@@ -10,7 +10,7 @@ export default async function JobsPage() {
       <Hero />
       <section className="items-start justify-start gap-10 p-10 md:flex">
         <Filter />
-        <div className="flex max-w-3xl flex-1 flex-col gap-5 py-10 md:py-0">
+        <div className="flex flex-col max-w-3xl gap-5 py-10 md:py-0">
           {[1, 2, 3].map((_, i) => (
             <JobCard
               key={i + i}
@@ -26,6 +26,8 @@ export default async function JobsPage() {
                 Distinctio, sapiente pariatur iure dolorem nam."
             />
           ))}
+
+          <Pagination />
         </div>
       </section>
       {/* <Image src="/collaby-logo.svg" width={200} height={200} alt="logo" /> */}

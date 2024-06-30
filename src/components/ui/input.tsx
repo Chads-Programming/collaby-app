53842a007d2997cd95b3b6d1e1c7cac6aacaf9c2
Arturo Iván Artaza <aartazaesc@gmail.com>
2024-06-29 23:10:18 -0300

    fix(ui): kirbe mistages (really my mistakes but its better name kirbe

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,17 +1,5 @@
-<<<<<<< Updated upstream
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
-
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-=======
-import * as React from "react";
-
 import { cn } from "@/lib/utils";
+import React from "react";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -20,36 +8,21 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, variant = "default", ...props }, ref) => {
->>>>>>> Stashed changes
     return (
       <input
         type={type}
         className={cn(
-<<<<<<< Updated upstream
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-=======
           variant === "no-style"
             ? ""
             : "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
->>>>>>> Stashed changes
         )}
         ref={ref}
         {...props}
       />
-<<<<<<< Updated upstream
-    )
-  }
-)
-Input.displayName = "Input"
-
-export { Input }
-=======
     );
   },
 );
 Input.displayName = "Input";
 
 export { Input };
->>>>>>> Stashed changes

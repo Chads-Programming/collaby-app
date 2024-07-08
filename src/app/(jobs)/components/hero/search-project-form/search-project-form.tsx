0491cc8d8f3aad553b5398ecd1730f71b8b17ca2
FOWMind <alwl1@hotmail.com>
2024-07-08 13:20:41 -0300

    style(hero): search input height

--- src/app/(jobs)/components/hero/search-project-form/search-project-form.tsx
+++ src/app/(jobs)/components/hero/search-project-form/search-project-form.tsx
@@ -1,58 +1,57 @@
-"use client";
-import React from "react";
-import { Input } from "@/components/ui/input";
-import { MultiSelect } from "@/components/ui/multi-select";
-import { Search } from "lucide-react";
-import { Controller, useForm } from "react-hook-form";
-import { TECHNOLOGIES } from "@/lib/constants";
-import { Button } from "@/components/ui/button";
+'use client'
+import React from 'react'
+import { Input } from '@/components/ui/input'
+import { MultiSelect } from '@/components/ui/multi-select'
+import { Search } from 'lucide-react'
+import { Controller, useForm } from 'react-hook-form'
+import { TECHNOLOGIES } from '@/lib/constants'
+import { Button } from '@/components/ui/button'
 
 export default function SearchProjectForm() {
-  const { register, watch, control, handleSubmit } = useForm({
-    defaultValues: {
-      technologies: [],
-      projectName: "",
-    },
-  });
-  const state = watch();
-  const onFormSubmit = (data: unknown) => {
-    //TODO: add filter when kirbe finish stuff things (im pretty sure this can't be done soon.)
-  };
-  return (
-    <form
-      onSubmit={handleSubmit(onFormSubmit)}
-      className="z-20 flex flex-col gap-3 md:flex-row"
-    >
-      <search className="flex h-16 w-full items-center gap-2 rounded-md border border-input bg-white px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
-        <Search className="text-gray-300" />
-        <Input
-          {...register("projectName")}
-          variant="no-style"
-          value={state.projectName}
-          placeholder="What kind of project are you looking for?"
-          className="w-full text-black outline-none"
-        />
-      </search>
-      <Controller
-        name="technologies"
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <MultiSelect
-            placeholder="Technologies"
-            options={TECHNOLOGIES.map((tech) => ({
-              value: tech,
-              label: tech,
-            }))}
-            onChange={(val) => {
-              console.log(val, value);
-              onChange([...value, val]);
-            }}
-            onBlur={onBlur}
-            value={value}
-          />
-        )}
-      />
-      <Button color="primary" size="xl">Search project</Button>
-    </form>
-  );
+	const { register, watch, control, handleSubmit } = useForm({
+		defaultValues: {
+			technologies: [],
+			projectName: '',
+		},
+	})
+	const state = watch()
+	const onFormSubmit = (data: unknown) => {
+		//TODO: add filter when kirbe finish stuff things (im pretty sure this can't be done soon.)
+	}
+	return (
+		<form onSubmit={handleSubmit(onFormSubmit)} className="z-20 flex flex-col gap-3 md:flex-row">
+			<search className="flex h-16 w-full items-center gap-2 rounded-md border border-input bg-white px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
+				<Search className="text-gray-300" />
+				<Input
+					{...register('projectName')}
+					variant="no-style"
+					value={state.projectName}
+					placeholder="What kind of project are you looking for?"
+					className="w-full text-black outline-none"
+				/>
+			</search>
+			<Controller
+				name="technologies"
+				control={control}
+				render={({ field: { onChange, onBlur, value } }) => (
+					<MultiSelect
+						placeholder="Technologies"
+						options={TECHNOLOGIES.map((tech) => ({
+							value: tech,
+							label: tech,
+						}))}
+						onChange={(val) => {
+							console.log(val, value)
+							onChange([...value, val])
+						}}
+						onBlur={onBlur}
+						value={value}
+					/>
+				)}
+			/>
+			<Button color="primary" size="xl">
+				Search project
+			</Button>
+		</form>
+	)
 }
--- src/components/ui/multi-select.tsx
+++ src/components/ui/multi-select.tsx
@@ -1,159 +1,143 @@
-"use client";
+'use client'
 
-import * as React from "react";
-import { X } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
-import {
-  Command,
-  CommandGroup,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import { Command as CommandPrimitive } from "cmdk";
+import * as React from 'react'
+import { X } from 'lucide-react'
+import { Badge } from '@/components/ui/badge'
+import { Command, CommandGroup, CommandItem, CommandList } from '@/components/ui/command'
+import { Command as CommandPrimitive } from 'cmdk'
 
 interface Option {
-  value: string;
-  label: string;
+	value: string
+	label: string
 }
 
 interface MultiSelectProps {
-  open?: boolean;
-  onOpenChange?: (value: boolean) => void;
-  placeholder?: string;
-  value?: Option[];
-  onBlur?: () => void;
-  onFocus?: () => void;
-  options: Option[];
-  onChange: (value: Option) => void;
+	open?: boolean
+	onOpenChange?: (value: boolean) => void
+	placeholder?: string
+	value?: Option[]
+	onBlur?: () => void
+	onFocus?: () => void
+	options: Option[]
+	onChange: (value: Option) => void
 }
 export function MultiSelect({
-  placeholder,
-  value,
-  options = [],
-  open: isOpen,
-  onChange,
-  onBlur,
-  onFocus,
+	placeholder,
+	value,
+	options = [],
+	open: isOpen,
+	onChange,
+	onBlur,
+	onFocus,
 }: MultiSelectProps) {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const [open, setOpen] = React.useState(isOpen);
-  const [selected, setSelected] = React.useState<Option[]>(value ?? []);
-  const [inputValue, setInputValue] = React.useState("");
+	const inputRef = React.useRef<HTMLInputElement>(null)
+	const [open, setOpen] = React.useState(isOpen)
+	const [selected, setSelected] = React.useState<Option[]>(value ?? [])
+	const [inputValue, setInputValue] = React.useState('')
 
-  const handleUnselect = React.useCallback((option: Option) => {
-    setSelected((prev) => prev.filter((s) => s.value !== option.value));
-  }, []);
+	const handleUnselect = React.useCallback((option: Option) => {
+		setSelected((prev) => prev.filter((s) => s.value !== option.value))
+	}, [])
 
-  const handleKeyDown = React.useCallback(
-    (e: React.KeyboardEvent<HTMLDivElement>) => {
-      const input = inputRef.current;
-      if (input) {
-        if (e.key === "Delete" || e.key === "Backspace") {
-          if (input.value === "") {
-            setSelected((prev) => {
-              const newSelected = [...prev];
-              newSelected.pop();
-              return newSelected;
-            });
-          }
-        }
-        // This is not a default behaviour of the <input /> field
-        if (e.key === "Escape") {
-          input.blur();
-        }
-      }
-    },
-    [],
-  );
+	const handleKeyDown = React.useCallback((e: React.KeyboardEvent<HTMLDivElement>) => {
+		const input = inputRef.current
+		if (input) {
+			if (e.key === 'Delete' || e.key === 'Backspace') {
+				if (input.value === '') {
+					setSelected((prev) => {
+						const newSelected = [...prev]
+						newSelected.pop()
+						return newSelected
+					})
+				}
+			}
+			// This is not a default behaviour of the <input /> field
+			if (e.key === 'Escape') {
+				input.blur()
+			}
+		}
+	}, [])
 
-  const selectables = React.useMemo(
-    () =>
-      options.filter(
-        (option) => !selected.some((sel) => sel.value === option.value),
-      ),
-    [selected, options],
-  );
+	const selectables = React.useMemo(
+		() => options.filter((option) => !selected.some((sel) => sel.value === option.value)),
+		[selected, options],
+	)
 
-  return (
-    <Command
-      onKeyDown={handleKeyDown}
-      className="overflow-visible bg-transparent"
-    >
-      <div className="group rounded-md border border-input px-3 py-2 text-sm ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2">
-        <div className="flex flex-wrap gap-1">
-          {selected.map((option) => {
-            return (
-              <Badge key={option.value} variant="secondary">
-                {option.label}
-                <button
-                  className="ml-1 rounded-full outline-none ring-offset-background focus:ring-2 focus:ring-ring focus:ring-offset-2"
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleUnselect(option);
-                    }
-                  }}
-                  onMouseDown={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                  }}
-                  onClick={() => handleUnselect(option)}
-                >
-                  <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
-                </button>
-              </Badge>
-            );
-          })}
-          {/* Avoid having the "Search" Icon */}
-          <CommandPrimitive.Input
-            ref={inputRef}
-            value={inputValue}
-            onValueChange={setInputValue}
-            onBlur={(e) => {
-              setOpen(false);
-              if (onBlur) {
-                onBlur();
-              }
-            }}
-            onFocus={(e) => {
-              setOpen(true);
-              if (onFocus) {
-                onFocus();
-              }
-            }}
-            placeholder={placeholder}
-            className="ml-2 flex-1 bg-transparent outline-none placeholder:text-muted-foreground"
-          />
-        </div>
-      </div>
-      <div className="relative mt-2">
-        <CommandList>
-          {open && selectables.length > 0 ? (
-            <div className="absolute top-0 z-10 w-full rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in">
-              <CommandGroup className="h-full max-h-[100px] overflow-auto">
-                {selectables.map((option) => {
-                  return (
-                    <CommandItem
-                      key={option.value}
-                      onMouseDown={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                      }}
-                      onSelect={(value) => {
-                        setInputValue("");
-                        onChange(option);
-                        setSelected((prev) => [...prev, option]);
-                      }}
-                      className={"cursor-pointer"}
-                    >
-                      {option.label}
-                    </CommandItem>
-                  );
-                })}
-              </CommandGroup>
-            </div>
-          ) : null}
-        </CommandList>
-      </div>
-    </Command>
-  );
+	return (
+		<Command onKeyDown={handleKeyDown} className="overflow-visible bg-transparent">
+			<div className="group rounded-md border border-input px-3 py-2 text-sm ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2">
+				<div className="flex flex-wrap gap-1 h-12">
+					{selected.map((option) => {
+						return (
+							<Badge key={option.value} variant="secondary">
+								{option.label}
+								<button
+									className="ml-1 rounded-full outline-none ring-offset-background focus:ring-2 focus:ring-ring focus:ring-offset-2"
+									onKeyDown={(e) => {
+										if (e.key === 'Enter') {
+											handleUnselect(option)
+										}
+									}}
+									onMouseDown={(e) => {
+										e.preventDefault()
+										e.stopPropagation()
+									}}
+									onClick={() => handleUnselect(option)}>
+									<X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
+								</button>
+							</Badge>
+						)
+					})}
+					{/* Avoid having the "Search" Icon */}
+					<CommandPrimitive.Input
+						ref={inputRef}
+						value={inputValue}
+						onValueChange={setInputValue}
+						onBlur={(e) => {
+							setOpen(false)
+							if (onBlur) {
+								onBlur()
+							}
+						}}
+						onFocus={(e) => {
+							setOpen(true)
+							if (onFocus) {
+								onFocus()
+							}
+						}}
+						placeholder={placeholder}
+						className="ml-2 flex-1 bg-transparent outline-none placeholder:text-muted-foreground"
+					/>
+				</div>
+			</div>
+			<div className="relative mt-2">
+				<CommandList>
+					{open && selectables.length > 0 ? (
+						<div className="absolute top-0 z-10 w-full rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in">
+							<CommandGroup className="h-full max-h-[100px] overflow-auto">
+								{selectables.map((option) => {
+									return (
+										<CommandItem
+											key={option.value}
+											onMouseDown={(e) => {
+												e.preventDefault()
+												e.stopPropagation()
+											}}
+											onSelect={(value) => {
+												setInputValue('')
+												onChange(option)
+												setSelected((prev) => [...prev, option])
+											}}
+											className={'cursor-pointer'}>
+											{option.label}
+										</CommandItem>
+									)
+								})}
+							</CommandGroup>
+						</div>
+					) : null}
+				</CommandList>
+			</div>
+		</Command>
+	)
 }

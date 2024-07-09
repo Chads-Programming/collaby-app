a8416a41b5d13fcfd9f11d65dba759b9d52c0363
FOWMind <alwl1@hotmail.com>
2024-07-09 06:24:57 -0300

    feat(jobs): casaving progress..., my pc is about to explode

--- src/app/(jobs)/components/job-card/job-card.tsx
+++ src/app/(jobs)/components/job-card/job-card.tsx
@@ -1,92 +1,96 @@
-import { Heading, Paragraph, Tag } from "@/app/components";
-import { cn } from "@/lib/utils";
-import { Calendar, CircleDollarSign, MapPin } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import { Heading, Paragraph, Tag } from '@/app/components';
+import { cn } from '@/lib/utils';
+import { Calendar, CircleDollarSign, MapPin } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 interface JobCardProps extends React.HTMLAttributes<HTMLDivElement> {
-  logo: string;
-  platform: string;
-  title: string;
-  slug: string;
-  location: string;
-  budget: string;
-  description: string;
-  date: Date;
-  isNew?: boolean;
+	logo: string;
+	platform: string;
+	title: string;
+	slug: string;
+	location: string;
+	budget: string;
+	description: string;
+	date: Date;
+	isNew?: boolean;
 }
 
 export const JobCard = ({
-  logo,
-  platform,
-  title,
-  slug,
-  location,
-  budget,
-  description,
-  date,
-  isNew,
-  className,
-  ...props
+	logo,
+	platform,
+	title,
+	slug,
+	location,
+	budget,
+	description,
+	date,
+	isNew,
+	className,
+	...props
 }: JobCardProps) => {
-  return (
-    <article
-      className={cn(
-        "items-start justify-start gap-5 bg-white p-5 shadow md:flex",
-        className,
-      )}
-      {...props}
-    >
-      <div className="mb-5 flex items-center justify-start gap-2 md:mb-0 md:block">
-        {/* <Image */}
-        {/*   src={logo} */}
-        {/*   width={100} */}
-        {/*   height={100} */}
-        {/*   className="w-10 md:w-24" */}
-        {/*   alt={"Logo"} */}
-        {/* /> */}
+	return (
+		<article className={cn('items-start justify-start gap-5 bg-white p-5 shadow md:flex', className)} {...props}>
+			<div className="mb-5 flex items-center justify-start gap-2 md:mb-0 md:block">
+				{/* <Image */}
+				{/*   src={logo} */}
+				{/*   width={100} */}
+				{/*   height={100} */}
+				{/*   className="w-10 md:w-24" */}
+				{/*   alt={"Logo"} */}
+				{/* /> */}
 
-        <img src={logo} className="w-10 md:w-24" alt={"Logo"} />
-        <Paragraph className="text-lg font-medium md:hidden">
-          {platform}
-        </Paragraph>
-      </div>
-      <div>
-        <Paragraph className="mb-2 hidden font-medium md:block">
-          {platform}
-        </Paragraph>
-        <div className="flex flex-col flex-wrap items-start justify-center gap-3 md:flex-row md:items-center md:justify-start">
-          <Link href={`/project/${slug}`} className="md:order-0 order-1">
-            <Heading>{title}</Heading>
-          </Link>
-          {isNew && <Tag className="order-0 md:order-1">New post</Tag>}
-        </div>
-        <div className="mt-2 flex flex-wrap items-start gap-5 md:gap-10">
-          <Paragraph className="font-medium text-neutral-600">
-            <MapPin
-              className="-mt-1 mr-1 inline-block align-middle"
-              size={15}
-            />
-            {location}
-          </Paragraph>
-          <Paragraph className="font-medium text-neutral-600">
-            <CircleDollarSign
-              className="-mt-1 mr-1 inline-block align-middle"
-              size={15}
-            />
-            {budget}k
-          </Paragraph>
-          <Paragraph className="font-medium text-neutral-600">
-            <Calendar
-              className="-mt-1 mr-1 inline-block align-middle"
-              size={15}
-            />
-            {date.toLocaleDateString()}
-          </Paragraph>
-        </div>
+				<img src={logo} className="w-10 md:w-24" alt={'Logo'} />
+				<Paragraph className="text-lg font-medium md:hidden">{platform}</Paragraph>
+			</div>
+			<div>
+				<Paragraph className="mb-2 hidden font-medium md:block">{platform}</Paragraph>
+				<div className="flex flex-col flex-wrap items-start justify-center gap-3 md:flex-row md:items-center md:justify-start">
+					<Link href={`/project/${slug}`} className="md:order-0 order-1">
+						<Heading>{title}</Heading>
+					</Link>
+					{isNew && <Tag className="order-0 md:order-1">New post</Tag>}
+				</div>
 
-        <Paragraph className="mt-2 text-neutral-600">{description}</Paragraph>
-      </div>
-    </article>
-  );
+				<JobCard.Tags location={location} budget={budget} date={date} />
+
+				<Paragraph className="mt-2 text-neutral-600">{description}</Paragraph>
+			</div>
+		</article>
+	);
 };
+
+interface JobCardTagsProps
+	extends Pick<JobCardProps, 'location' | 'budget' | 'date'>,
+		React.HTMLAttributes<HTMLDivElement> {
+	textClassName?: string;
+}
+
+const JobTags = ({ location, budget, date, className, textClassName, ...props }: JobCardTagsProps) => {
+	return (
+		<div className={cn('mt-2 flex flex-wrap items-start gap-5 md:gap-10', className)} {...props}>
+			<JobCardTagsParagraph className={textClassName}>
+				<MapPin className="-mt-1 mr-1 inline-block align-middle" size={15} />
+				{location}
+			</JobCardTagsParagraph>
+			<JobCardTagsParagraph className={textClassName}>
+				<CircleDollarSign className="-mt-1 mr-1 inline-block align-middle" size={15} />
+				{budget}k
+			</JobCardTagsParagraph>
+			<JobCardTagsParagraph className={textClassName}>
+				<Calendar className="-mt-1 mr-1 inline-block align-middle" size={15} />
+				{date.toLocaleDateString()}
+			</JobCardTagsParagraph>
+		</div>
+	);
+};
+
+interface JobCardTagsParagraphProps extends React.HTMLAttributes<HTMLParagraphElement> {}
+
+const JobCardTagsParagraph = ({ className, children, ...props }: JobCardTagsParagraphProps) => (
+	<Paragraph className={cn('font-medium text-neutral-600', className)} {...props}>
+		{children}
+	</Paragraph>
+);
+
+JobCard.Tags = JobTags;
--- src/app/(jobs)/project/[slug]/page.tsx
+++ src/app/(jobs)/project/[slug]/page.tsx
@@ -1,15 +1,40 @@
-import { Heading } from "@/app/components";
+import { Heading } from '@/app/components';
+import { JobCard } from '@/app/(jobs)/components';
+import Image from 'next/image';
 
 interface ProjectPageProps {
-  params: {
-    slug: string;
-  };
+	params: {
+		slug: string;
+	};
 }
 
+const getProject = () => ({
+	logo: 'https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png',
+	platform: 'Notion',
+	title: 'Software Engineer',
+	location: 'Madrid',
+	budget: '50-80',
+	description:
+		'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex soluta incidunt explicabo fugit assumenda earum aut harum, non eius? Distinctio, sapiente pariatur iure dolorem nam.',
+	date: new Date(),
+	isNew: true,
+});
+
 export default function ProjectPage({ params: { slug } }: ProjectPageProps) {
-  return (
-    <header>
-      <Heading>Project page. Slug: {slug}</Heading>
-    </header>
-  );
+	const { logo, title, platform, location, budget, description, date, isNew } = getProject();
+	return (
+		<main className="p-5">
+			<header className="flex justify-start items-stretch gap-5 bg-secondary rounded-xl p-5">
+				<div className="p-5 bg-primary rounded-xl">
+					<Image src={logo} width={100} height={100} alt={platform} />
+				</div>
+				<div>
+					<Heading size="lg">{title}</Heading>
+					<div>
+						<JobCard.Tags budget={budget} date={date} location={location} />
+					</div>
+				</div>
+			</header>
+		</main>
+	);
 }
